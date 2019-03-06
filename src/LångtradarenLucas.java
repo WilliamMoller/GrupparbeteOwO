9d7cc181d81ff8fc27b0f581bb034dc9911a9eca
Michael1100 <michael.gabrielsson.te16d@gmail.com>
2019-03-06 11:56:11 +0100

    extends

--- src/LångtradarenLucas.java
+++ src/LångtradarenLucas.java
@@ -1,5 +1,11 @@
+import javafx.scene.paint.Color;
 
-public class LångtradarenLucas {
+public class LångtradarenLucas extends Car{
+	
+	public LångtradarenLucas() {
+		super(600, "LångtradarenLucas", Color.BROWN);
+		stopEngine();
 
 	
 }
+	}
