418368db3b727e47848396eaec96d0437876fa32
WilliamMoller <william.moller.te16d@gmail.com>
2019-03-27 12:03:38 +0100

    kommentarer

--- src/Car.java
+++ src/Car.java
@@ -2,6 +2,9 @@ import javafx.scene.paint.Color;
 
 public class Car extends Vehicle{
 
+	/**
+	 * Constructor som ger bilar hästkrafter, namn och färg
+	 */
 	protected Car(double ep, String m, Color c) {
 		super(ep, m, c);
 		// TODO Auto-generated constructor stub
--- src/Volvo740.java
+++ src/Volvo740.java
@@ -4,6 +4,9 @@ public class Volvo740 extends Car{
 
 	public final static double trimFactor = 1.25;
 
+	/**
+	 * Självaste bilen. Den i sin tur får hästkrafter, namn och färg
+	 */
 	public Volvo740() {
 		super(100, "Volvo740" , Color.RED);
 		stopEngine();
